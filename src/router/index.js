9f58a0b266dc3886be36f068ed8ab0d0f28295df
Elektropay <richard.r@everpayinc.com>
2019-06-27 06:48:35 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -79,7 +79,7 @@ export const constantRoutes = [
         path: 'dashboard',
         component: () => import('@/views/dashboard/index'),
         name: 'Dashboard',
-        meta: { title: 'Activity', icon: 'dashboard', affix: true }
+        meta: { title: 'Dashboard', icon: 'dashboard', affix: true }
       }
     ]
   },
