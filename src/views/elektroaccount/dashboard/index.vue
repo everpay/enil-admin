656c265a449396b6c71f2bcab86a71a5b5fc3585
Richard Rowe <richard.r@everpayinc.com>
2019-06-27 06:12:27 -0500

    6th commit
    Edited elektroaccount views folder

---
+++ src/views/elektroaccount/dashboard/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="dashboard-container">
+    <component :is="currentRole" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import adminDashboard from './admin'
+import editorDashboard from './editor'
+import editorDashboard from './personal'
+import editorDashboard from './business'
+
+export default {
+  name: 'Dashboard',
+  components: { adminDashboard, editorDashboard, personalDashboard, businessDashboard },
+  data() {
+    return {
+      currentRole: 'businessDashboard'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'roles'
+    ])
+  },
+  created() {
+    if (!this.roles.includes('business')) {
+      this.currentRole = 'personalDashboard'
+    }
+  }
+}
+</script>
