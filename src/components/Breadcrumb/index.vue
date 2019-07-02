4e42b037adf91cf02e595c68a2afb3557552e182
Elektropay <richard.r@everpayinc.com>
2019-07-02 09:48:46 -0500

    Update index.vue

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -3,7 +3,7 @@
     <transition-group name="breadcrumb">
       <el-breadcrumb-item v-for="(item,index) in levelList" :key="item.path">
         <span v-if="item.redirect==='noRedirect'||index==levelList.length-1" class="no-redirect">{{ item.meta.title }}</span>
-        <a style="display:none;" v-else @click.prevent="handleLink(item)">{{ item.meta.title }}</a>
+        <a v-else style="display:none;" @click.prevent="handleLink(item)">{{ item.meta.title }}</a>
       </el-breadcrumb-item>
     </transition-group>
   </el-breadcrumb>
